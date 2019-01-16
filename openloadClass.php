979c304eaade3338945467f19444bda12b010950
ToishY <31921460+ToishY@users.noreply.github.com>
2019-01-16 02:28:45 +0100

    Add files via upload

---
+++ openloadClass.php
@@ -0,0 +1,130 @@
+<?php
+   /**
+   * PHP class for Openload/Streamango API
+   * 
+   * @author     ToishY
+   * @version    1.0.1
+   */
+	class OpenloadMain{
+		private $log;
+		private $key;
+		public $host;
+
+		function __construct($apiHost, $apiLogin, $apiKey){
+			$this->host = $apiHost; #https://api.openload || https://api.fruithosted.net
+			$this->log = $apiLogin;
+			$this->key = $apiKey; 
+		}
+
+		public function curlBuilder($functionName, $functionArgs = NULL){
+			$ch = curl_init();
+			// call requested method
+			$requestUrl = $this->$functionName($functionArgs);
+			// setopts
+			curl_setopt($ch, CURLOPT_URL, $requestUrl);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+			curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+			$response = curl_exec($ch);
+			if (!curl_errno($ch)) {
+				$res = json_decode($response,true);
+			}else{
+				$res = curl_error($ch);
+			}
+			curl_close($ch);
+			return $res;
+		}
+
+		private function accountInfo(){
+			return $this->host.'/account/info?'.$this->queryBuilder();
+		}
+
+		private function fileInfo($inputArray){
+			//$inputArray = array("file"=>"d5e6f7g8,h9i10j11k")
+			return $this->host.'/file/info?'.$this->queryBuilder($inputArray);
+		}
+
+		private function uploadServer($inputArray){
+			//$inputArray = array("folder"=>0,"sha1"=>NULL,"httponly"=>NULL)
+			return $this->host.'/file/ul?'.$this->queryBuilder($inputArray);
+		}
+
+		public function uploadFile($inputArray){
+			//$inputArray = array('C:\Users\foo\bar.mp4',0,NULL,NULL)
+			list($filePath, $folderId, $sha, $http) = $inputArray;
+
+			// Get upload server
+			$vjson = $this->curlBuilder("uploadServer", array("folder"=>$folderId,"sha1"=>$sha,"httponly"=>$http));
+
+			// POST variables
+			$postFile = ['file1' => new \CurlFile($filePath, mime_content_type($filePath), basename($filePath))];
+
+		    // Upload file
+		    $ch = curl_init($vjson["result"]["url"]);
+		    curl_setopt($ch, CURLOPT_POSTFIELDS, $postFile);
+		    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+		    // Set execution time to inf
+		    ini_set('max_execution_time', 0);
+		    $res = json_decode(curl_exec($ch),true);
+		    if (($res['msg'] !== 'OK')) {
+		        $ares = array("code"=>400,"response"=>$res,"curl_error"=>curl_error($ch));
+		    }else{
+		    	$ares = array("code"=>200,"response"=>$res);
+		    }
+		    curl_close($ch);
+		    return $ares;
+		}
+
+		private function uploadByURL($inputArray){
+			//$inputArray = array("url"=>"http://clips.vorwaerts-gmbh.de/VfE_html5.mp4","folder"=>NULL,"headers"=>NULL)
+			return $this->host.'/remotedl/add?'.$this->queryBuilder($inputArray);
+		}
+
+		private function checkRemoteUpload($inputArray){
+			//$inputArray = array("limit"=>50,"id"=>NULL)
+			return $this->host.'/remotedl/status?'.$this->queryBuilder($inputArray);
+		}
+
+		private function folderList($folderId = NULL){
+			//$inputArray = array("folder"=>0)
+			return $this->host.'/file/listfolder?'.$this->queryBuilder($inputArray);
+		}
+
+		private function folderRename($inputArray){
+			//$inputArray = array("folder"=>123,"name"=>"my awesome folder v2")
+			return $this->host.'/file/renamefolder?'.$this->queryBuilder($inputArray);
+		}
+
+		private function fileRename($inputArray){
+			//$inputArray = array("file"=>"h9i10j11k","name"=>"my awesome file v2")
+			return $this->host.'/file/rename?'.$this->queryBuilder($inputArray);
+		}
+
+		private function deleteFile($inputArray){
+			//$inputArray = array("file"=>"h9i10j11k")
+			return $this->host.'/file/delete?'.$this->queryBuilder($inputArray);
+		}
+
+		private function convertFile($inputArray){
+			//$inputArray = array("file"=>"d5e6f7g8")
+			return $this->host.'/file/convert?'.$this->queryBuilder($inputArray);
+		}
+
+		private function checkRunningConverts($inputArray){
+			//$inputArray = array("folder"=>NULL)
+			return $this->host.'/file/runningconverts?'.$this->queryBuilder($inputArray);
+		}
+
+		private function getSplashImage($inputArray){
+			//$inputArray = array("file"=>"d5e6f7g8")
+			return $this->host.'/file/getsplash?'.$this->queryBuilder($inputArray);
+		}
+
+		private function queryBuilder($query = NULL){
+			$qar = (isset($query) ? array_merge(array("login"=>$this->log,"key"=>$this->key),$query) : array("login"=>$this->log,"key"=>$this->key));
+			return http_build_query($qar, '', '&', PHP_QUERY_RFC3986);
+		}
+	}
+?>
---
+++ opl-how2use.php
@@ -0,0 +1,121 @@
+<?php
+# HOW TO USE OPENLOAD CLASS
+require_once 'openloadClass.php';
+
+# Pass your API login & key from the OPENLOAD/STREAMANGO user panel in here
+$opl = new OpenloadMain("https://api.openload.co/1","y29psa10mv73x02s","f00b4rz");
+
+# Get account info
+$res = $opl->curlBuilder("accountInfo");
+print_r($res);
+
+# Get file info
+/* Arguments
+- Required: 
+	FileCode(s); if multiple files, please comma seperate
+*/
+$res = $opl->curlBuilder("fileInfo",array("file"=>"d5e6f7g8,h9i10j11k");
+print_r($res);
+
+# Get upload server
+/* Arguments (in array):
+- Optional
+	FolderId; folder id; 0 = root
+	Sha1; expected sha1
+	Http; only use http upload links if set to true
+*/
+$res = $opl->curlBuilder("uploadServer",array("folder"=>0,"sha1"=>NULL,"httponly"=>NULL));
+print_r($res);
+
+# Upload file
+/* Arguments (in array):
+- Required
+	FilePath; the full path of the file you want to upload
+	!IMPORTANT please use single quotes for filepath
+- Optional
+	FolderId;
+	Sha1; expected sha1
+	Http; only use http upload links if set to true
+*/
+$res = $opl->uploadFile(array('C:\Users\foo\bar.mp4',0,NULL,NULL));
+print_r($res);
+
+# Upload by URL
+/* Arguments (in array):
+- Required
+	VideoURL; link to video
+- Optional
+	FolderId;
+	Headers; optional headers
+*/
+$res = $opl->curlBuilder("uploadByURL",array("url"=>"http://clips.vorwaerts-gmbh.de/VfE_html5.mp4","folder"=>NULL,"headers"=>NULL));
+print_r($res);
+
+# Check remote upload status 
+/* Arguments (in array):
+- Optional
+	Limit; amount of files to show (default 5; max 100)
+	RemoteId; remote upload id
+*/
+$res = $opl->curlBuilder("checkRemoteUpload",array("limit"=>50,"id"=>NULL));
+print_r($res);
+
+# Show folders content
+/* Arguments:
+- Optional
+	FolderId;
+*/
+$res = $opl->curlBuilder("folderList",array("folder"=>0));
+print_r($res);
+
+# Rename folder
+/* Arguments (in array):
+- Required
+	FolderId;
+	FolderName; new name of the folder id specified
+*/
+$res = $opl->curlBuilder("folderRename",array("folder"=>123,"name"=>"my awesome folder v2"));
+print_r($res);
+
+# Rename file
+/* Arguments (in array):
+- Required
+	FileCode;
+	FileName; new name of the file id specified
+
+*/
+$res = $opl->curlBuilder("fileRename",array("file"=>"h9i10j11k","name"=>"my awesome file v2"));
+print_r($res);
+
+# Delete file
+/* Arguments (in array):
+- Required
+	FileCode;
+*/
+$res = $opl->curlBuilder("deleteFile",array("file"=>"h9i10j11k"));
+print_r($res);
+
+# Convert file
+/* Arguments:
+- Required
+	FileCode;
+*/
+$res = $opl->curlBuilder("convertFile",array("file"=>"d5e6f7g8"));
+print_r($res);
+
+# Check running conversion
+/* Arguments:
+- Optional
+	FolderId;
+*/
+$res = $opl->curlBuilder("checkRunningConverts",array("folder"=>NULL));
+print_r($res);
+
+# Get splash image
+/* Arguments:
+- Required
+	FileCode;
+*/
+$res = $opl->curlBuilder("getSplashImage",array("file"=>"d5e6f7g8"));
+print_r($res);	
+?>
